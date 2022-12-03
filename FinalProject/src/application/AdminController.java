708cb415eb7dd5c0eca52ceba78b6d908c2d3bee
Likithagopi01 <gopi.l@northeastern.edu>
2022-12-02 19:01:40 -0500

    Admin Controller

---
+++ FinalProject/src/application/AdminController.java
@@ -0,0 +1,44 @@
+package application;
+
+import edu.northeastern.models.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AdminController {
+
+    @FXML
+    private Button ViewTrip;
+
+    @FXML
+    private Button addTrip;
+
+    @FXML
+    private Label adminfName;
+
+    @FXML
+    private Label adminlName;
+
+    @FXML
+    private Button delupTrip;
+
+    @FXML
+    void add_Trip(ActionEvent event) {
+    
+    }
+
+    @FXML
+    void delup_Trip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void view_Trip(ActionEvent event) {
+
+    }
+    
+    
+
+
+}
